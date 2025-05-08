eb8a3253f0ee66746be0125c7d350dcc82cae352
Thamer Alanazi <thamer51513@gmail.com>
2025-05-08 10:04:28 +0300

    ADD

---
+++ main.js
@@ -0,0 +1,20 @@
+let size = 12;
+let text = document.getElementById("text");
+function sizeadd() {
+    text.style.fontSize = `${size++}px`;
+}
+
+function sizede() {
+    taxt.style.fontSize = `${size--}px`;
+}
+function textChang() {
+    text.innerText="Fahad";
+}
+function changBack() {
+    text.style.backgroundColor = "blue"
+}
+function colerchag() {
+    text.style.color = "green"
+}
+
+
