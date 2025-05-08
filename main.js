17912147894a6414da9d08d64d7e0da0bae4d14a
Thamer Alanazi <thamer51513@gmail.com>
2025-05-08 10:15:10 +0300

    ADD

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 let size = 12;
 let text = document.getElementById("text");
 function sizeadd() {
-    text.style.fontSize = `${size++}px`;
+    size += 4
+    text.style.fontSize = `${size}px`;
 }
 
 function sizede() {
-    taxt.style.fontSize = `${size--}px`;
+    size -= 4
+    taxt.style.fontSize = `${size}px`;
 }
 function textChang() {
     text.innerText="Fahad";
@@ -16,5 +18,14 @@ function changBack() {
 function colerchag() {
     text.style.color = "green"
 }
+let divimg = document.getElementById("img-div");
+function imgadd(){
+    let img = document.createElement("img");
+    img.src = "https://images.pexels.com/photos/31829770/pexels-photo-31829770/free-photo-of-solitary-red-tulip-in-lush-green-meadow.jpeg?auto=compress&cs=tinysrgb&w=600&lazy=load"
+    divimg.appendChild(img);
+}
 
+function hideimg(){
+    divimg.innerText = "";
+}
 
